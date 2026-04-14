93fa166190d30c876e57b438c8aea9cf9d12b252
Lautaro Marchesi <159946877+LHMarchesi@users.noreply.github.com>
2026-04-14 12:39:01 -0300

    Cheking collisions

--- Assets/_Scripts/Player/Movement.cs
+++ Assets/_Scripts/Player/Movement.cs
@@ -68,4 +68,9 @@ public class Movement : MonoBehaviour
         );
     
 }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collided with: " + collision.gameObject.name);
+    }
 }
