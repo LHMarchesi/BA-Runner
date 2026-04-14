92dd37214cb099019febdf75bc098e7923320065
Lautaro Marchesi <159946877+LHMarchesi@users.noreply.github.com>
2026-04-14 00:37:13 -0300

    Initialize

---
+++ Assets/_Scripts/Movement.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    private int currentCarPosition;
+    private int maxPosition = 4;
+    [SerializeField] float laneOffset;
+    [SerializeField] Transform startPosition;
+
+    private void Start()
+    {
+        targetPosition = startPosition.position;
+        transform.position = targetPosition;
+    }
+
+    private bool canMove = true;
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if (!context.started || !canMove) return;
+
+        Vector2 input = context.ReadValue<Vector2>();
+        float vertical = input.y;
+
+        if (vertical > 0 && currentCarPosition < maxPosition)
+        {
+            currentCarPosition++;
+            Move();
+        }
+        else if (vertical < 0 && currentCarPosition > 0)
+        {
+            currentCarPosition--;
+            Move();
+        }
+    }
+
+    private void Move()
+    {
+        canMove = false;
+        UpdatePosition();
+        Invoke(nameof(ResetMove), 0.1f); // cooldown
+    }
+
+    private void ResetMove()
+    {
+        canMove = true;
+    }
+
+
+    private Vector3 targetPosition;
+
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, targetPosition, 10f * Time.deltaTime);
+    }
+
+    private void UpdatePosition()
+    {
+        targetPosition = new Vector3(
+            startPosition.position.x,
+            startPosition.position.y + currentCarPosition * laneOffset,
+            startPosition.position.z
+        );
+    
+}
+}
