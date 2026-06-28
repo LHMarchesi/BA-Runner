52edcb08a1109b7e642a27d0136f9e0e984117b2
Lautaro Marchesi <159946877+LHMarchesi@users.noreply.github.com>
2026-06-28 16:39:14 -0300

    DemoBuild

---
+++ Assets/_Scripts/DialogueGraph/Editor/DialogueNodes.cs
@@ -0,0 +1,73 @@
+#if UNITY_EDITOR
+using UnityEngine;
+using Unity.GraphToolkit.Editor;
+using System;
+
+[Serializable]
+public class StartNode : Node
+{
+    protected override void OnDefinePorts(IPortDefinitionContext context)
+    {
+        context.AddOutputPort("out").Build();
+    }
+}
+
+
+[Serializable]
+public class EndNode : Node
+{
+    protected override void OnDefinePorts(IPortDefinitionContext context)
+    {
+        context.AddInputPort("in").Build();
+    }
+}
+
+
+[Serializable]
+public class DialogueNode : Node
+{
+
+    protected override void OnDefinePorts(IPortDefinitionContext context)
+    {
+        context.AddInputPort("in").Build();
+        context.AddOutputPort("out").Build();
+
+        context.AddInputPort<string>("Speaker").Build();
+        context.AddInputPort<string>("Dialogue").Build();
+        context.AddInputPort<Sprite>("Image").Build();
+        context.AddInputPort<float>("Delay").Build();
+    }
+}
+
+
+[Serializable]
+public class ChoiceNode : Node
+{
+    const string optionID = "portCount";
+
+    protected override void OnDefinePorts(IPortDefinitionContext context)
+    {
+        context.AddInputPort("in").Build();
+
+        context.AddInputPort<string>("Speaker").Build();
+        context.AddInputPort<string>("Dialogue").Build();
+        context.AddInputPort<Sprite>("Image").Build();
+
+        var option = GetNodeOptionByName(optionID);
+        option.TryGetValue(out int portCount);
+        for (int i = 0; i < portCount; i++)
+        {
+            context.AddInputPort<string>($"Choice {i} Text").Build();
+            context.AddInputPort<string>($"Choice {i} Flags").Build();
+            context.AddInputPort<string>($"Choice {i} ClearFlags").Build();
+            context.AddOutputPort($"Choice {i}").Build();
+        }
+    }
+
+    protected override void OnDefineOptions(IOptionDefinitionContext context)
+    {
+        context.AddOption<int>(optionID);
+    }
+}
+#endif
+
