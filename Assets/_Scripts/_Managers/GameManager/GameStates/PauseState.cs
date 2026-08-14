6ea51a197889ec4ef4fb1b9e8fd1cad258d0e2a6
dansleif <logonza22@gmail.com>
2026-08-14 06:22:44 -0300

    Bugfix, menú nav progress, sound nav

--- Assets/_Scripts/_Managers/GameManager/GameStates/PauseState.cs
+++ Assets/_Scripts/_Managers/GameManager/GameStates/PauseState.cs
@@ -3,28 +3,23 @@ using UnityEngine;
 public class PauseState : IState
 {
     GameManager gm;
-
     public PauseState(GameManager gm)
     {
         this.gm = gm;
     }
-
     public void Awake()
     {
         Time.timeScale = 0f;
-
+        AudioListener.pause = true;
         EventBus<OnPauseEvent>.Raise(new OnPauseEvent { isPaused = true });
     }
-
     public void Execute()
     {
-
     }
-
     public void Sleep()
     {
         Time.timeScale = 1f;
-
+        AudioListener.pause = false;
         EventBus<OnPauseEvent>.Raise(new OnPauseEvent { isPaused = false });
     }
 }
